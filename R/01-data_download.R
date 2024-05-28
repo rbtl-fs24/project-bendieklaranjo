28987bbfe3552483d38ea62952b4f29a8caf8d2a
bendieklaranjo <ana.ombenlar@gmail.com>
2024-05-28 13:49:14 +0000

    Read CSV succesfully

--- R/01-data_download.R
+++ R/01-data_download.R
@@ -2,5 +2,4 @@ library(tidyverse)
 library(googlesheets4)
 library(dplyr)
 
-waste_streams_zh <- read_sheet("https://docs.google.com/spreadsheets/d/18MvqHvZcvM74AEhgdKB7_YZIkWBWmqm5Ek14pqm9uoY/edit?usp=sharing")
-write_csv(waste_streams_zh, "data/raw/waste-streams_zh_raw.csv")
+eth_dep_sus_visibility_raw <- read_sheet("https://docs.google.com/spreadsheets/d/18MvqHvZcvM74AEhgdKB7_YZIkWBWmqm5Ek14pqm9uoY/edit?usp=sharing")
